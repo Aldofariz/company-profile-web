9011e7006b29a1f48942aeaa3fcccc8829ae5bc8
unknown <you@example.com>
2025-06-03 22:47:19 +0700

    initial project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <h1 className='text-4xl font-bold text-blue-400'>Hello, Developers</h1>
+    </>
+  )
+}
+
+export default App
